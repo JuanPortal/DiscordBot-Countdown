b07edc992bb34e16fe6175fad89bbce1d1b45833
Juan Portal <78442505+JuanPortal@users.noreply.github.com>
2023-04-23 13:18:33 -0700

    Updated for the new Discord Bot requirements

--- Countdown.py
+++ Countdown.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import discord
 import time
 
-client = commands.Bot(command_prefix="$")
+client = commands.Bot(command_prefix="$", intents=discord.Intents.all())
 
 client.remove_command("help")
 
